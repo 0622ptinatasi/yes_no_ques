9f8955c23025036ba366ab364b52dacd6fbfb5b4
hw2 <tinatina62027@gmail.com>
2025-01-02 14:18:47 +0800

    Initial Commit

---
+++ yes_no_ques/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  yes_no_ques
+//
+//  Created by user12 on 2025/1/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ yes_no_ques/yes_no_quesApp.swift
@@ -0,0 +1,17 @@
+//
+//  yes_no_quesApp.swift
+//  yes_no_ques
+//
+//  Created by user12 on 2025/1/2.
+//
+
+import SwiftUI
+
+@main
+struct yes_no_quesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
